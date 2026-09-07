86dfac64d8c38c17e8412aa00460c7ecee66f100
SHS PC <lorensborleyo@gmail.com>
2026-09-07 14:39:24 +0800

    added user mini peta profile (Pelonio)

---
+++ app/src/test/java/com/example/queue_lesshealth/Pelonio_Minipeta1.java
@@ -0,0 +1,27 @@
+package com.example.queue_lesshealth;
+
+import org.junit.Test;
+
+public class Pelonio_Minipeta1 {
+    @Test
+    package com.example.queue_lesshealth;
+
+import org.junit.Test;
+
+    public class MiniPeta1_sample {
+        @Test
+        public void printMyProfile() {
+            // --- 1. THE INPUT (Storing your personal details in variables)---
+            String myName = "Ethan";
+            String petName = "Hades";
+            String favFood = "Nuggets";
+            int myAge = 16;
+
+            // --- 2. THE OUTPUT (Printing to the console) ---
+            System.out.println("--- MY DIGITAL PROFILE ---");
+            System.out.println("Hello, my name is " + myName + " and i am " + myAge + " years old.");
+            System.out.println("I have a wonderful pet named " + petName + ".");
+            System.out.println("If I could, I would eat " + favFood + " every single day!");
+        }
+    }
+}
