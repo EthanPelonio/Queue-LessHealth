510cfa1925cbecff32c268b9299fcc7c30401d99
SHS PC <ethanbradley909@gmail.com>
2026-09-14 13:18:47 +0800

    added minipeta 3

--- app/src/test/java/com/example/queue_lesshealth/quarter2/Buendia_Minipeta1.java
+++ app/src/test/java/com/example/queue_lesshealth/quarter2/Buendia_Minipeta1.java
@@ -1,4 +1,4 @@
-package com.example.queuelesshealth;
+package com.example.queue_lesshealth.quarter2;
 
 import org.junit.Test;
 
--- app/src/test/java/com/example/queue_lesshealth/quarter2/Pelonio_MiniPeta1.java
+++ app/src/test/java/com/example/queue_lesshealth/quarter2/Pelonio_MiniPeta1.java
@@ -1,4 +1,4 @@
-package com.example.queue_lesshealth;
+package com.example.queue_lesshealth.quarter2;
 
 import org.junit.Test;
 
