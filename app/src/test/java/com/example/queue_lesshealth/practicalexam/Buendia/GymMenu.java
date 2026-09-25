a2eb4b02226e4e64c24b826aa23655061a6f916b
keziah2009 <kayleighkiehl23@gmail.com>
2026-09-25 14:01:06 +0800

    Commit 1 - Gym classes

---
+++ app/src/test/java/com/example/queue_lesshealth/practicalexam/Buendia/GymMenu.java
@@ -0,0 +1,14 @@
+package com.example.queue_lesshealth.practicalexam.Buendia;
+import java.util.Scanner;
+import java.util.Scanner;
+
+public class GymMenu {
+
+    public void start(Scanner scanner) {
+
+        boolean running = true;
+
+
+    }
+}
+
